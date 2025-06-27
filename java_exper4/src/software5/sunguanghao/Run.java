757770185f29b1d4c0ee5f82ac176d377f8eef63
git config --global user.name "To-Elysia-1 <git config --global user.email "utp674lrf@outlook.com>
2025-06-28 07:53:33 +0800

    final version

--- java_exper4/src/software5/sunguanghao/Run.java
+++ java_exper4/src/software5/sunguanghao/Run.java
@@ -14,6 +14,7 @@ public class Run {
     public static int choice;
     public static int scope;
     public static int top=0,low=0;
+    public static int wrong_right=0;
     public static AddArithmetic add=new AddArithmetic();
     public static SubArithmetic sub=new SubArithmetic();
     public static MulArithmetic mul=new MulArithmetic();
@@ -50,83 +51,93 @@ public class Run {
 
     }
     public static void wrong_op(){
-        Scanner scanner=new Scanner(System.in);
-        int a,b;
+        JFrame frame=new JFrame("错误题目");
+        java.util.List<JTextField>answerGields=new ArrayList<>();
+        ArrayList<Integer> rsu=new ArrayList<>();
+
         int wrong=remain-count_right;
-        System.out.println("正确率是 "+(double)count_right/remain*100+"%");
-        System.out.println("错误的题目数是 "+wrong);
-        if(wrong!=0) {
-            System.out.println("继续做错误的题目吗?");
-            int cotiue;
-            cotiue = scanner.nextInt();
-            if (cotiue == 1) {
-                for (String tmp : wrong_test) {
-                    char ch;
-                    int i = 0;
-                    a = b = 0;
-                    while (tmp.charAt(i) >= '0' && tmp.charAt(i) <= '9') {
-                        a = a * 10 + tmp.charAt(i) - '0';
-                        i++;
-                    }
-                    ch = tmp.charAt(i++);
-                    while (i < tmp.length()) {
-                        b = b * 10 + tmp.charAt(i) - '0';
-                        i++;
-                    }
-                    switch (ch) {
-
-                        case '+': {
-                            add.set_int(a, b);
-                            add.show_int();
-                            int rsu = scanner.nextInt();
-                            if (add.judge_int(rsu)) {
-                                System.out.println("Right");
-
-                            } else System.out.println("Wrong");
-                            break;
-                        }
-                        case '-': {
-                            sub.set_int(a, b);
-                            sub.show_int();
-                            int rsu = scanner.nextInt();
-                            if (sub.judge_int(rsu)) {
-                                System.out.println("Right");
-
-                            } else System.out.println("Wrong");
-                            break;
-                        }
-                        case '*': {
-                            mul.set_int(a, b);
-                            mul.show_int();
-                            int rsu = scanner.nextInt();
-                            if (mul.judge_int(rsu)) {
-                                System.out.println("Right");
-
-                            } else System.out.println("Wrong");
-                            break;
-                        }
-                        case '/': {
-                            div.set_int(a, b);
-                            div.show_int();
-                            int rsu = scanner.nextInt();
-                            if (div.judge_int(rsu)) {
-                                System.out.println("Right");
-
-                            } else System.out.println("Wrong");
-                            break;
-                        }
-                    }
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,600);
+        frame.setLayout(new BorderLayout());
+        JPanel panel=new JPanel(new GridLayout(wrong,2,10, 10));
+        for(String title:wrong_test){
+            JLabel label = new JLabel(title);
+            JTextField field=new JTextField(5);
+            JPanel qPannel=new JPanel();
+            qPannel.add(label);
+            qPannel.add(field);
+            panel.add(qPannel);
+            answerGields.add(field);
+        }
+        JButton nextBtn=new JButton("完成");
+        nextBtn.addActionListener(e-> {
+            for(int i=0;i<wrong;i++){
+                String text;
+                text=answerGields.get(i).getText().trim();
+                int ans=Integer.parseInt(text);
+                if(ans==wrong_calculate(wrong_test.get(i))){
+                    wrong_right++;
                 }
             }
-        }
+            frame.dispose();
+            JOptionPane.showMessageDialog(null, "错误题目完成！正确率是："+
+                    String.valueOf((double)wrong_right/wrong*100)+"%"+'\n'+"错误的题目数是 "+
+                    String.valueOf(wrong - wrong_right));
+            System.exit(0);
+        });
+        frame.add(panel, BorderLayout.CENTER);
+        frame.add(nextBtn, BorderLayout.SOUTH);
+        frame.setVisible(true);
     }
-    public static void op(){
+    public static int wrong_calculate(String tmp){
+
+            char ch;
+            int i = 0;
+            int a = 0;
+            int b=0;
+            int rsu=0;
+            while (tmp.charAt(i) >= '0' && tmp.charAt(i) <= '9') {
+                a = a * 10 + tmp.charAt(i) - '0';
+                i++;
+            }
+            ch = tmp.charAt(i++);
+            while (i < tmp.length()) {
+                b = b * 10 + tmp.charAt(i) - '0';
+                i++;
+            }
+            switch (ch) {
+
+                case '+': {
+                    add.set_int(a, b);
+                    //add.show_int();
+                    rsu= a+b;
+                }
+                case '-': {
+                    sub.set_int(a, b);
+                    //sub.show_int();
+                    rsu= a-b;
+                }
+                case '*': {
+                    mul.set_int(a, b);
+                    //mul.show_int();
+                    rsu= a*b;
+                }
+                case '/': {
+                    div.set_int(a, b);
+                    //div.show_int();
+                    rsu= a/b;
+                }
+            }
+        return rsu;
+    }
+    public static ArrayList<String> op(int need,ArrayList<Integer> rsu){
         int a,b;
-        int result;
+
+        ArrayList<String> Title = new ArrayList<>();
         Scanner scanner=new Scanner(System.in);
         Random random=new Random();
 
-        for(int i=0;i<remain;i++)
+        for(int i=0;i<need;i++)
         {
             if(flag==0)
                 choice=random.nextInt(4);
@@ -143,18 +154,19 @@ public class Run {
                     add.add_Vector.get(0).add(a);
                     add.add_Vector.get(1).add(b);
                     add.set_int(a, b);
-                    add.show_int();
-                    result=scanner.nextInt();
-                    if(add.judge_int(result))
-                    {
-                        System.out.println("Right");
-                        count_right++;
-                    }
-                    else {
-                        System.out.println("Wrong");
-                        wrong_test.add(String.valueOf(a)+'+'+String.valueOf(b));
-                    }
-
+                    //add.show_int();
+                    Title.add(String.valueOf(a)+'+'+String.valueOf(b));
+//                    result=scanner.nextInt();
+//                    if(add.judge_int(result))
+//                    {
+//                        System.out.println("Right");
+//                        count_right++;
+//                    }
+//                    else {
+//                        System.out.println("Wrong");
+//                        wrong_test.add(String.valueOf(a)+'+'+String.valueOf(b));
+//                    }
+                    rsu.add(a+b);
                     break;
 
                 }
@@ -169,17 +181,19 @@ public class Run {
                     sub.sub_Vector.get(0).add(a);
                     sub.sub_Vector.get(1).add(b);
                     sub.set_int(a, b);
-                    sub.show_int();
-                    result=scanner.nextInt();
-                    if(sub.judge_int(result))
-                    {
-                        System.out.println("Right");
-                        count_right++;
-                    }
-                    else {
-                        System.out.println("Wrong");
-                        wrong_test.add(String.valueOf(a)+'-'+String.valueOf(b));
-                    }
+                    //sub.show_int();
+                    Title.add(String.valueOf(a)+'-'+String.valueOf(b));
+//                    result=scanner.nextInt();
+//                    if(sub.judge_int(result))
+//                    {
+//                        System.out.println("Right");
+//                        count_right++;
+//                    }
+//                    else {
+//                        System.out.println("Wrong");
+//                        wrong_test.add(String.valueOf(a)+'-'+String.valueOf(b));
+//                    }
+                    rsu.add(a-b);
                     break;
 
                 }
@@ -194,17 +208,19 @@ public class Run {
                     mul.mul_Vector.get(0).add(a);
                     mul.mul_Vector.get(1).add(b);
                     mul.set_int(a, b);
-                    mul.show_int();
-                    result=scanner.nextInt();
-                    if(mul.judge_int(result))
-                    {
-                        System.out.println("Right");
-                        count_right++;
-                    }
-                    else {
-                        System.out.println("Wrong");
-                        wrong_test.add(String.valueOf(a)+'*'+String.valueOf(b));
-                    }
+                    //mul.show_int();
+                    Title.add(String.valueOf(a)+'*'+String.valueOf(b));
+//                    result=scanner.nextInt();
+//                    if(mul.judge_int(result))
+//                    {
+//                        System.out.println("Right");
+//                        count_right++;
+//                    }
+//                    else {
+//                        System.out.println("Wrong");
+//                        wrong_test.add(String.valueOf(a)+'*'+String.valueOf(b));
+//                    }
+                    rsu.add(a*b);
                     break;
 
                 }
@@ -219,24 +235,26 @@ public class Run {
                     div.div_Vector.get(0).add(0);
                     div.div_Vector.get(1).add(b);
                     div.set_int(a, b);
-                    div.show_int();
-                    result=scanner.nextInt();
-                    if(div.judge_int(result))
-                    {
-                        System.out.println("Right");
-                        count_right++;
-                    }
-                    else {
-                        System.out.println("Wrong");
-                        wrong_test.add(String.valueOf(a)+'/'+String.valueOf(b));
-                    }
+                    //div.show_int();
+                    Title.add(String.valueOf(a)+'/'+String.valueOf(b));
+//                    result=scanner.nextInt();
+//                    if(div.judge_int(result))
+//                    {
+//                        System.out.println("Right");
+//                        count_right++;
+//                    }
+//                    else {
+//                        System.out.println("Wrong");
+//                        wrong_test.add(String.valueOf(a)+'/'+String.valueOf(b));
+//                    }
+                    rsu.add(a/b);
                     break;
 
 
                 }
             }
         }
-        wrong_op();
+        return Title;
     }
     public static void init_show(){
         java.util.List<JTextField> answerFields = new ArrayList<>();
@@ -295,7 +313,7 @@ public class Run {
 
 
                 frame.dispose(); // 关闭配置界面
-                op();
+                show();
                 // run(flag, choice, scope, remain, top, low); // 进入下一阶段
 
             } catch (NumberFormatException ex) {
@@ -310,27 +328,61 @@ public class Run {
         frame.setVisible(true);
 
     }
-    public static void show(int a,int b,char op){
+    public static void show(){
         JFrame frame = new JFrame("题目页 " + (currentIndex / 10 + 1));
+        java.util.List<JTextField> answerFields = new ArrayList<>();
+        ArrayList<Integer>rsu=new ArrayList<>();
         int need_show=Math.min(remain-currentIndex, 10);
+        ArrayList<String> Titles = op(need_show,rsu);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500, 600);
         frame.setLayout(new BorderLayout());
-        JPanel panel = new JPanel(new GridLayout(need_show, 1, 10, 10));
-        JLabel label = new JLabel((currentIndex + 1) + ". " + a + op + b + " = ");
-        JTextField answer = new JTextField(5);
-        JPanel qPanel = new JPanel();
-        qPanel.add(label);
-        qPanel.add(answer);
-        panel.add(qPanel);
-        currentIndex++;
+        JPanel panel = new JPanel(new GridLayout(need_show, 2, 10, 10));
+        for(String title:Titles){
+            JLabel label=new JLabel(title);
+            JTextField field = new JTextField(5);
+            JPanel qPanel = new JPanel();
+            qPanel.add(label);
+            qPanel.add(field);
+            panel.add(qPanel);
+            answerFields.add(field);
+            currentIndex++;
+        }
+
+
         JButton nextBtn = new JButton(currentIndex < remain ? "下一页" : "提交");
         nextBtn.addActionListener(e -> {
+
+            for(int i=0;i<need_show;i++){
+                String text;
+                text = answerFields.get(i).getText().trim();
+                int ans=Integer.parseInt(text);
+                if(ans!= rsu.get(i)){
+                    wrong_test.add(Titles.get(i));
+                }
+                else{
+                    count_right++;
+                }
+            }
+
             frame.dispose(); // 关闭当前窗口
             if (currentIndex < remain) {
                 // 显示下一页
+                show();
             } else {
-                JOptionPane.showMessageDialog(null, "题目完成！");
+                int result = JOptionPane.showConfirmDialog(
+                        null,
+                        "题目完成！正确率是：" + String.valueOf((double)count_right / remain * 100) + "%" + '\n' +
+                                "错误的题目数是 " + String.valueOf(remain - count_right),
+                        "结果",
+                        JOptionPane.OK_CANCEL_OPTION
+                );
+
+                if (remain-count_right!=0&&result == JOptionPane.OK_OPTION) {
+                    wrong_op();
+                } else {
+                    System.exit(0);
+                }
             }
         });
 
